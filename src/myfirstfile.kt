923b3d3977513afd885c2fbd6800984f5e315672
Marlon Monzon <martianworm17@gmail.com>
2018-08-27 19:55:42 +0800

    Kotlin Hello World: How it works? Part-2

--- src/myfirstfile.kt
+++ src/myfirstfile.kt
@@ -1,4 +1,9 @@
 // Unit means no return value. Void method.
 fun main(args: Array<String>) : Unit {
-    print("Hello World")
+    println("Hello World")
+    println("Hello Kotlin")
+    println(10 - 9)
+    println(10 / 9)
+    println(10.0 / 9)
+    println(true)
 }
