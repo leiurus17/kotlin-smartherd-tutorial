b78393c072cedd259b4eea698d3c8daad385113f
Marlon Monzon <martianworm17@gmail.com>
2018-08-27 20:18:10 +0800

    #3.1 Kotlin Basic Syntax Tutorial: COMMENTS Part-1

--- src/myfirstfile.kt
+++ src/myfirstfile.kt
@@ -1,9 +1,21 @@
-// Unit means no return value. Void method.
-fun main(args: Array<String>) : Unit {
-    println("Hello World")
-    println("Hello Kotlin")
-    println(10 - 9)
-    println(10 / 9)
-    println(10.0 / 9)
-    println(true)
+/**
+ * This is the main function. Entry point of the application
+ * Unit means no return value. Void method.
+ *
+ */
+fun main(args: Array<String>) : Unit { // This is an inline comment...
+    var myString = "Hello World"
+    var myNumber = 10           // Int
+    var myDecimcal = 1.0        // Decimal
+
+    var myAnotherString: String // String
+    myAnotherString = "Hello Kotlin"
+
+    val myFinalString = "My Constant String Value" // val means final. Immutable.
+
+    println(myString)
+    println(myNumber)
+    println(myDecimcal)
+    println(myAnotherString)
+    println(myFinalString)
 }
