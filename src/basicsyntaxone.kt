91b7489ad8ac6406b1465ea1063aca2e4061c97c
Marlon Monzon <martianworm17@gmail.com>
2018-08-31 22:07:55 +0800

    String interpolation

--- src/basicsyntaxone.kt
+++ src/basicsyntaxone.kt
@@ -4,7 +4,10 @@ fun main(args: Array<String>) {
     var personObj = Person()
     personObj.name = "Steve"
 
-    print("The name of the person is : " + personObj.name)
+    println("The name of the person is : " + personObj.name)
+    println("The name of the person is : ${personObj.name}") // String interpolation
+    println("The name of the person is : $personObj.name")
+
 }
 
 class Person {
