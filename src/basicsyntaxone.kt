0bca4832d154eccaa8fd8ca60425622c79f3c146
Marlon Monzon <martianworm17@gmail.com>
2018-08-31 22:14:08 +0800

    #3.4 Kotlin Basics and Syntax: FUNCTIONS CLASS & STRING INTERPOLATION Part-4

--- src/basicsyntaxone.kt
+++ src/basicsyntaxone.kt
@@ -1,21 +1,10 @@
+import com.shreks.Person
+
 fun main(args: Array<String>) {
 
 
-    var personObj = Person()
-    personObj.name = "Steve"
+    var personObj = Person("Steve")
+    personObj.display()
 
-    println("The name of the person is : " + personObj.name)
-    println("The name of the person is : ${personObj.name}") // String interpolation
-    println("The name of the person is : $personObj.name")
-
-}
-
-class Person {
-
-    var name: String = ""
-
-    fun display() {
-        print(name)
-    }
 }
 
---
+++ src/com/shreks/Person.kt
@@ -0,0 +1,8 @@
+package com.shreks
+
+class Person(var name: String) {
+
+    fun display() {
+        print("The name of the person is ${name}")
+    }
+}
